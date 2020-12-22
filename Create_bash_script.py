e013e0ae238eb0aded0df9180dca770a78ae610e
piyush <piyushgoel997@gmail.com>
2020-12-22 18:33:36 -0500

    debug script creator script

--- Create_bash_script.py
+++ Create_bash_script.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     path = args.path
 
-    names = set([str(f).split(".")[0] for f in listdir(path) if isfile(join(path, f)) and "log" not in f])
+    names = set([str(f).split(".")[0]
+                 for f in listdir(path) if isfile(join(path, f)) and "log" not in f and "fc" not in f])
     commands = "\n".join(["echo \"" + n + "\"\npython Main_2.py --data " + n for n in names])
     f = open("run_all", 'w')
     f.write(commands)

2e5400365f50efd2769047db051a822d3e860a70
piyush <piyushgoel997@gmail.com>
2020-12-22 17:26:39 -0500

    python script to create bash script to run all experiments

---
+++ Create_bash_script.py
@@ -0,0 +1,15 @@
+import argparse
+from os import listdir
+from os.path import isfile, join
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path')
+    args = parser.parse_args()
+    path = args.path
+
+    names = set([str(f).split(".")[0] for f in listdir(path) if isfile(join(path, f)) and "log" not in f])
+    commands = "\n".join(["echo \"" + n + "\"\npython Main_2.py --data " + n for n in names])
+    f = open("run_all", 'w')
+    f.write(commands)

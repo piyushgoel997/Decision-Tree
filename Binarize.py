30dc0c01958adc550d59fd7f9936d7de613cade0
piyush <piyushgoel997@gmail.com>
2020-12-23 14:21:23 -0500

    add condition filter out single valued features

--- Binarize.py
+++ Binarize.py
@@ -44,59 +44,8 @@ def binarize_classes(classes):
     return result
 
 
-##########################
-# f = pd.read_csv("DataSets/abalone/abalone.data", header=None)
-# features = [binarize_feature(f[0])]
-# for i in range(1, 8):
-#     _f = f[i]
-#     if i == 3: features.append(binarize_feature(_f, divs=7))
-#     else: features.append(binarize_feature(_f, divs=4))
-# features.append(np.array(f[8]).reshape((len(f[8]), 1)))
-# data = np.concatenate(features, axis=1)
-# np.save("DataSets/abalone.npy", data)
-###########################
-
-##########################
-# f = pd.read_csv("DataSets/Anuran Calls (MFCCs)/Frogs_MFCCs.csv")
-# features = []
-# for i in list(f.columns)[:-4]:
-#     _f = f[i]
-#     features.append(binarize_feature(_f, divs=5))
-# output_features = []
-# for i in list(f.columns)[-4:-1]:
-#     _f = f[i]
-#     output_features.append(np.array(_f))
-# data = np.concatenate(features, axis=1)
-# print(data.shape)
-# for i, o in enumerate(output_features):
-#     o = o.reshape((len(o), 1))
-#     print(o.shape)
-#     np.save("DataSets/frogs" + str(i) + ".npy", np.concatenate((data, o), axis=1))
-##########################
-
-##########################
-# f = pd.read_csv("DataSets/New folder/bank-full.csv", delimiter=';')
-# cols = f.columns
-# features = [binarize_feature(f[cols[0]], divs=4)]
-# for i in range(1, 5):
-#     features.append(binarize_feature(f[cols[i]]))
-# features.append(binarize_feature(f[cols[5]], divs=4))
-# for i in range(6, 9):
-#     features.append(binarize_feature(f[cols[i]]))
-# features.append(binarize_feature(f[cols[11]], divs=4))
-# # 12 -> 15 incl
-# for i in range(12, 15):
-#     if i == 14: continue
-#     features.append(binarize_feature(f[cols[i]], divs=4))
-# features.append(binarize_feature(f[cols[15]]))
-# features.append(np.array(f[cols[-1]]).reshape((len(f[cols[-1]]), 1)))
-# data = np.concatenate(features, axis=1)
-# print(data.shape)
-# np.save("DataSets/bank.npy", data)
-###########################
-
-name = "car"
-file_name = "DataSets/New folder/" + name + ".data"
+name = "faults"
+file_name = "DataSets/New folder/New Folder/" + name + ".csv"
 sys.stdout = open("DataSets/" + name + "_log.txt", 'w')
 f = pd.read_csv(file_name, header=None, delimiter=',')
 cols = f.columns
@@ -110,6 +59,8 @@ for c in cols[:split_at]:
         features.append(binarize_feature(f[c], divs=10))
     elif num_feature_values == 2:
         features.append(binarize_feature(f[c], divs=2))
+    elif num_feature_values == 1:
+        continue
     else:
         features.append(binarize_feature(f[c]))
     f_c.append(f_c[-1] + features[-1].shape[1])
